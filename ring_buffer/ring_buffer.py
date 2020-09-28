06dda2d129299700421369e2d7b3edf3f7633d62
Pedro Escobedo <pedro.escobedo.design@gmail.com>
2020-09-27 20:15:02 -0700

    sprint done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity = 3):
+        self.capacity = capacity
+        self.size = 0
+        self.first_value = 0
+        self.data = []
 
-    def append(self, item):
-        pass
+    def append(self, value):
+        if self.size >= self.capacity:
+            self.data[self.first_value] = value
+        else:
+            self.data.append(value)
+            self.size += 1
+
+        if self.first_value < (self.capacity -1):
+            self.first_value += 1
+        else:
+            self.first_value = 0
 
     def get(self):
-        pass
+        return self.data
